d3335e91dca2e44f48d3bd1fd231d4392fc8e065
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-01-29 23:49:18 +0530

    Time: 8 ms (37.67%), Space: 8.7 MB (8.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string updated_s = "";
-        for(int i = 0; i < s.size(); i++) {
-            if((isalpha(s[i])) || (isdigit(s[i]))) {
-                if(isupper(s[i]))
-                    updated_s += tolower(s[i]);
-                else
-                    updated_s += s[i];
+        int st = 0, e = s.size() - 1;
+        while(st <= e) {
+            if(!isalnum(s[st])) {
+                st++;
+                continue;
             }
+            if(!isalnum(s[e])) {
+                e--;
+                continue;
+            }
+            if(tolower(s[st]) != tolower(s[e])) {
+                return false;
+            }
+            st++;
+            e--;
         }
-        string rev = "";
-        for(int i = updated_s.size() - 1; i >= 0; i--) {
-            rev += updated_s[i];
-        }
+        return true;
+        // string updated_s = "";
+        // for(int i = 0; i < s.size(); i++) {
+        //     if((isalpha(s[i])) || (isdigit(s[i]))) {
+        //         if(isupper(s[i]))
+        //             updated_s += tolower(s[i]);
+        //         else
+        //             updated_s += s[i];
+        //     }
+        // }
+        // string rev = "";
+        // for(int i = updated_s.size() - 1; i >= 0; i--) {
+        //     rev += updated_s[i];
+        // }
 
-        return rev == updated_s ? true : false;
+        // return rev == updated_s ? true : false;
     }
 };
