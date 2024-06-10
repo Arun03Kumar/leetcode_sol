e9377a4a6f5232645af5b00ebfe9f0b318a9d697
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-06-11 02:24:14 +0530

    Time: 47 ms (65.84%), Space: 30 MB (10.87%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
-    string firstPalindrome(vector<string>& words) {
-        // string ans = "";
-        for(string x: words) {
-            // string temp = "";
-            // int l = x.size();
-            // while(--l >= 0) temp += x[l];
-            // // cout << temp << " ";
-            // if(temp == x) {
-            //     ans = x;
-            //     break;
-            // }
-            // int l = 0, r = x.size() - 1, flag = true;
-            // while(l <= r) {
-            //     if(x[l++] != x[r--]) {
-            //         flag = false;
-            //         break;
-            //     }
-            // }
-            // if(flag) return x;
-            for(int i=0; i<words.size(); i++){
-            int p =0;
-            int flag = true;
-            string nword = words[i];
-            int q = nword.length()-1;
-            while(p<q){
-                if(nword[p]!=nword[q]){
-                    flag = false;
-                }
-                p++;
-                q--;
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while(l <= r) {
+            if(s[l] == s[r]) {
+                l++;
+                r--;
             }
-            if(flag == true){
-                return words[i];
+            else {
+                return false;
             }
         }
-        return "";
+        return true;
+    }
+    string firstPalindrome(vector<string>& words) {
+        for(string x: words) {
+            if(isPalindrome(x))
+                return x;
         }
         return "";
     }
