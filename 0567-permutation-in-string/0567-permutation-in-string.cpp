f46295b68118456e4f97cb14672a1d779d82b4cc
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-06-14 01:42:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
+        vector<int> s1_hash(26, 0), s2_hash(26, 0);
+        for(int i = 0; i < s1.size(); i++) {
+            s1_hash[s1[i] - 'a']++;
+            s2_hash[s2[i] - 'a']++;
+        }
+
+        int l = 0, r = s1.size();
+        while(r < s2.size()) { 
+            if(s1_hash == s2_hash) return true;
+            s2_hash[s2[r] - 'a']++;
+            s2_hash[s2[l] - 'a']--;
+            l++;
+            r++;
+        }
+        return false;
+    }
+};
