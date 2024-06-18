fe7ba882c611bf2f92fe21aa40d5f2f3422b6801
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-06-18 15:56:01 +0530

    Time: 26 ms (16.31%), Space: 27.3 MB (82.09%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
+        // reverse whole array
+        //reverse starting k elements
+        //reverse last n-k elements.
+       int n = nums.size();
         if(k == 0) return;
-        k = k % n;
+        k = k % n; // we do this because k may be greater than n so if length of array is 4 and k is also 4 then it means we are moving the array 0 times.
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        reverse(nums.begin() + k, nums.end()); 
     }
 };
