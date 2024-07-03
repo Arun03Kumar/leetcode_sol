2a9678786076e02a1e3b204ad11f9627edf51242
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-07-03 17:33:08 +0530

    Time: 69 ms (56.53%), Space: 38.5 MB (90.81%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-        if(nums.size() <= 4) return 0;
-        else {
-            sort(nums.begin(), nums.end());
-            int l = nums[nums.size() - 1] - nums[3];
-            int r = nums[nums.size() - 4] - nums[0];
-            int l1 = nums[nums.size() - 3] - nums[1];
-            int r1 = nums[nums.size() - 2] - nums[2];
-            return min(l, min(r, min(l1, r1)));
-        }
+        // there will be only four cases:
+        //1. change the last 3 values to the minimum value in this case (n-4)th will be max and 0th will be minimum.
+        //2. either change the last two values equal to the (n-3)rd value and minimum value to the first value so this way minimum will be 1st value and maximum will be n-3rd value.
+        // 3. change last element to the second last and first two element to the third element.
+        //4. change the starting 3 elements to the 4th element.
+        // then find the minimum of all.
+        int n = nums.size();
+        if(n <= 4) return 0;
+        sort(nums.begin(), nums.end());
+        
+        int first_case = nums[n - 4] - nums[0];
+        int second_case = nums[n - 3] - nums[1];
+        int third_case = nums[n - 2] - nums[2];
+        int fourth_case = nums[n - 1] - nums[3];
+        return min(first_case, min(second_case, min(third_case, fourth_case)));  
     }
-    
 };
