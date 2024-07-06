f85527ac956231e9a7f663051deed28cb7a2d4d9
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-07-07 02:03:26 +0530

    Time: 27 ms (8.65%), Space: 18.3 MB (13.9%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool DFS(unordered_map<int, vector<int>> adj, vector<int> &colors, int node, int node_color) {
+    bool DFS(vector<vector<int>> adj, vector<int> &colors, int node, int node_color) {
         colors[node] = node_color;
         for(int x: adj[node]) {
             if(colors[x] == node_color) {
@@ -13,20 +13,55 @@ public:
         return true;
     }
 
-    bool isBipartite(vector<vector<int>>& graph) {
-        unordered_map<int, vector<int>> adj;
-        for(int i = 0; i < graph.size(); i++) {
-            for(int x: graph[i]) {
-                adj[i].push_back(x);
-            }
-        }
+    bool BFS(vector<vector<int>> adj, vector<int> &colors, int node, int node_color) {
+        queue<int> q;
+        q.push(node);
+        colors[node] = node_color;
 
-        vector<int> colors(graph.size(), -1);
-        for(int i = 0; i < graph.size(); i++) {
-            if(colors[i] == -1) {
-                if(!DFS(adj, colors, i, 0)) return false;
+        while(!q.empty()) {
+            int front = q.front();
+            q.pop();
+            
+            for(int x: adj[front]) {
+
+                if(colors[x] == colors[front]) return false;
+
+                if(colors[x] == -1) {
+                    q.push(x);
+                    colors[x] = 1 - colors[front];
+                }
             }
         }
         return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        // unordered_map<int, vector<int>> adj;
+        // for(int i = 0; i < graph.size(); i++) {
+        //     for(int x: graph[i]) {
+        //         adj[i].push_back(x);
+        //     }
+        // }
+
+        // no need to create an unordred map its already in the adjecency list form.
+
+        //DFS approach
+
+        // vector<int> colors(graph.size(), -1);
+        // for(int i = 0; i < graph.size(); i++) {
+        //     if(colors[i] == -1) {
+        //         if(!DFS(graph, colors, i, 0)) return false;
+        //     }
+        // }
+        // return true;
+
+        //BFS approach
+        vector<int> colors(graph.size(), -1);
+        for(int i = 0; i < graph.size(); i++) {
+            if(colors[i] == -1) {
+                if(!BFS(graph, colors, i, 0)) return false;
+            }
+        }
+        return true; 
     }
 };
