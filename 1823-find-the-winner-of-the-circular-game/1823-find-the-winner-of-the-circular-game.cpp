55b61e94cedf72a6b962dcc7298ffe7e81901426
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-07-08 14:17:21 +0530

    Time: 21 ms (28.58%), Space: 25.8 MB (9.19%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> arr;
+        // Approach 1: O(n^2)
+        // vector<int> arr;
+        // for(int i = 1; i <= n; i++) {
+        //     arr.push_back(i);
+        // }
+
+        // int i = 0;
+        // while(arr.size() > 1) { //O(n)
+        //     int idx = (i + k - 1) % arr.size();
+
+        //     arr.erase(arr.begin() + idx); // O(n) because of shifting
+
+        //     i = idx;
+        // }
+        // return arr[0];
+
+        // Approach 2: using queue O(n.k)
+        // add all elements into queue then pop k-1 elements and push them into the back of queue then remove the first element because that will now be the kth element which we want to remove
+
+        queue<int> q;
         for(int i = 1; i <= n; i++) {
-            arr.push_back(i);
+            q.push(i);
         }
 
-        int i = 0;
-        while(arr.size() > 1) {
-            int idx = (i + k - 1) % arr.size();
-
-            arr.erase(arr.begin() + idx);
-
-            i = idx;
+        while(q.size() > 1) {
+            for(int count = 1; count <= k - 1; count++) {
+                q.push(q.front()); // push back the k-1 elements to the q
+                q.pop();
+            }
+            q.pop(); // remove the kth element
         }
-        return arr[0];
+        return q.front();
+
     }
 };
