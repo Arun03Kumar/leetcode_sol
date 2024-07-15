438259f41162eeb381013d114e6ed3cae6257c1b
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-07-15 18:46:09 +0530

    Time: 182 ms (5.08%), Space: 135.8 MB (5.02%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -20,13 +20,46 @@ public:
         }
 
     }
+
+    void BFS(vector<vector<int>>& grid, int i, int j, int &ans) {
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        grid[i][j] = -1;
+
+        auto is_invalid = [&](int p1, int p2) {
+            return p1 < 0 || p1 >= grid.size() || p2 < 0 || p2 >= grid[0].size() || grid[p1][p2] == 0;
+        };
+
+        while(!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+
+            for(auto dir: directions) {
+                int new_x = x + dir[0]; 
+                int new_y = y + dir[1];
+
+                if(is_invalid(new_x, new_y)) {
+                    ans++;
+                }
+                else if(grid[new_x][new_y] == -1) {
+                    continue;
+                }
+                else {
+                    grid[new_x][new_y] = -1;
+                    q.push({new_x, new_y});
+                }
+            }
+        }
+    }
     int islandPerimeter(vector<vector<int>>& grid) {
         int ans = 0;
         vector<vector<int>> visited(grid.size(), vector<int>(grid[0].size(), 0));
         for(int i = 0; i < grid.size(); i++) {
             for(int j = 0; j < grid[0].size(); j++) {
                 if(grid[i][j] == 1) {
-                    DFS(grid, i, j, ans);
+                    // DFS(grid, i, j, ans);
+                    BFS(grid, i, j, ans);
                     return ans;
                 }
             }
