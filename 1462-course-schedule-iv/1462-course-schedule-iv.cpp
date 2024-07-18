0be5f3ec1e4cea1b6a83e38bd9b0e362f29af7ba
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-07-19 03:35:45 +0530

    Time: 324 ms (24.7%), Space: 148.2 MB (10.31%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    unordered_set<int> dfs(unordered_map<int, vector<int>> adj, int node, vector<int> &visited, unordered_map<int, unordered_set<int>> &indirect) {
+        visited[node] = 1;
+        
+        unordered_set<int> temp;
+        for(auto x: adj[node]) {
+            if(!visited[x]) {
+                unordered_set<int> returned = dfs(adj, x, visited, indirect);
+                temp.insert(x);
+                temp.insert(returned.begin(), returned.end());
+            }
+            else {
+                // unordered_set<int> t;
+                temp.insert(x);
+                temp.insert(indirect[x].begin(), indirect[x].end());
+                // return t;
+            }
+        }
+
+        indirect[node].insert(temp.begin(), temp.end());
+        return indirect[node];
+    }
+
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adj;
+        for(int i = 0; i < prerequisites.size(); i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            adj[v].push_back(u);
+        }
+
+        vector<int> visited(numCourses, 0);
+
+        unordered_map<int, unordered_set<int>> indirect;
+
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i]) {
+                dfs(adj, i, visited, indirect);
+            }
+        }
+
+        for(auto x: indirect) {
+            cout << x.first << "->";
+            for(auto y: x.second) {
+                cout << y << " ";
+            }
+            cout << endl;
+        }
+
+        vector<bool> ans;
+
+        for(auto x: queries) {
+            int v = x[1];
+            int u = x[0];
+
+            if(indirect[v].find(u) == indirect[v].end()) {
+                ans.push_back(false);
+            }
+            else {
+                ans.push_back(true);
+            }
+        }
+
+        return ans;
+
+    }
+};
