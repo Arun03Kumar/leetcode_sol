e52b1abf67150f078eb8dc05d0139f17c14266e4
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-07-24 17:57:23 +0530

    Time: 365 ms (5.07%), Space: 199.8 MB (5.07%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void bfs(unordered_map<int, vector<int>> &adj, int node, vector<int> &visited) {
+    void bfs(unordered_map<int, vector<int>> &adj, int node, vector<int> &visited, vector<int> &indegree) {
         queue<int> q;
         visited[node] = 1;
         q.push(node);
@@ -14,6 +14,7 @@ public:
                 if(!visited[x]) {
                     visited[x] = 1;
                     q.push(x);
+                    indegree[x]--;
                 }
             }
 
@@ -21,20 +22,22 @@ public:
     }
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
         unordered_map<int, vector<int>> adj;
+        vector<int> indegree(n, 0);
 
         for(auto edge: edges) {
             int u = edge[0];
             int v = edge[1];
 
             adj[u].push_back(v);
+            indegree[v]++;
         }
 
         vector<int> visited(n, 0);
         vector<int> ans;
 
         for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                bfs(adj, i, visited);
+            if(!visited[i] && indegree[i] == 0) {
+                bfs(adj, i, visited, indegree);
                 ans.push_back(i);
             }
         }
