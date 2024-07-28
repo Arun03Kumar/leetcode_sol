58662d833addf436625c37143050d96b1a15df63
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-07-29 03:21:46 +0530

    Time: 36 ms (57.66%), Space: 17.5 MB (57.94%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int recursive(vector<int> &coins, int amount) {
+        if(amount == 0) return 0;
+        if(amount < 0) return INT_MAX;
+
+        int ans = INT_MAX;
+        for(int i = 0; i < coins.size(); i++) {
+            int temp = recursive(coins, amount - coins[i]);
+            if(temp != INT_MAX) {
+                ans = min(ans, temp + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    int memoization(vector<int> &coins, int amount, vector<int> &dp) {
+
+        if(amount < 0) return INT_MAX;
+        if(amount == 0) return 0;
+
+        if(dp[amount] != INT_MAX) return dp[amount];
+
+        int min_coins = INT_MAX;
+        for(int i = 0; i < coins.size(); i++) {
+            int temp = memoization(coins, amount - coins[i], dp);
+            if(temp >= 0 && temp < min_coins) {
+                min_coins = temp + 1;
+            }      
+        }
+
+        dp[amount] = (min_coins == INT_MAX) ? -1 : min_coins;
+        return dp[amount];
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        
+        //Approach 1: recursion or backtracking
+
+        // int temp = recursive(coins, amount);
+        // if(temp == INT_MAX) return -1;
+        // else return temp;
+
+
+        // Approach 2: memoization
+        vector<int> dp(amount + 1, INT_MAX);
+        memoization(coins, amount, dp);
+        return dp[amount] != INT_MAX ? dp[amount] : 0;
+        // return dp[amount] != INT_MAX ? dp[amount] : -1;
+    }
+};
