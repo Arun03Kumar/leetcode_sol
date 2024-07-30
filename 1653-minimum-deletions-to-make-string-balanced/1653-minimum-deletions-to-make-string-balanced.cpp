80417a9e807a82aa34fc9f10cce3e6bd72ff46d6
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-07-30 21:04:06 +0530

    Time: 91 ms (60.29%), Space: 23.7 MB (66.99%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -4,23 +4,53 @@ public:
 
         // when we get a 'b' at the top of stack and our current character is 'a' then we need to remove these two characters from the stack, since we didn't even push the current character yet so just pop the top character in to the stack.
 
-        stack<char> st;
+        // stack<char> st;
 
-        int i = 0, count = 0;
-        while(i < s.size()) {
-            // char top = st.top();
-            char curr = s[i];
+        // int i = 0, count = 0;
+        // while(i < s.size()) {
+        //     // char top = st.top();
+        //     char curr = s[i];
 
-            if(!st.empty() && (curr == 'a' && st.top() == 'b')) {
-                st.pop();
-                count++;
+        //     if(!st.empty() && (curr == 'a' && st.top() == 'b')) {
+        //         st.pop();
+        //         count++;
+        //     }
+        //     else {
+        //         st.push(curr);
+        //     }
+        //     i++;
+        // }
+
+        // return count;
+
+
+        // Approach 2: O(1) solution
+
+        // in this we are trying to remove all the b's on the left side of an index and all the a's from the right side of the index, so we need a frequency array for both but we can reduce it to just a variable.
+
+        // in starting we count all the numbers of a and on each iteration we remove one if current element is 'a' and add to the b's variable if it is 1.
+
+        int right_a = 0;
+        int left_b = 0;
+
+        for(int i = 0 ; i < s.size(); i++) {
+            if(s[i] == 'a') {
+                right_a++;
             }
-            else {
-                st.push(curr);
-            }
-            i++;
         }
 
-        return count;
+        int ans = INT_MAX;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(s[i] == 'a') right_a--;
+
+            ans = min(ans, right_a + left_b);
+
+            if(s[i] == 'b') left_b++;
+
+        }
+
+        return ans;
     }
 };
