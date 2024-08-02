7d7071e4d6f51a29e0b2df8cc9988ce41738e651
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-08-03 02:50:14 +0530

    Time: 226 ms (92.26%), Space: 127 MB (18.24%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int recursion(vector<vector<int>>& questions, int idx) {
+        if(idx >= questions.size()) return 0;
+        
+        int points = questions[idx][0];
+        int brain_power = questions[idx][1];
+
+        int take = points + recursion(questions, idx + brain_power + 1);
+        // int take = 0;
+        // if(idx + brain_power < questions.size()) {
+        //     take = points + recursion(questions, idx + brain_power + 1);
+        // }
+
+        int skip = recursion(questions, idx + 1);
+
+        return max(take, skip);
+    }
+
+    long long memoization(vector<vector<int>>& q, vector<long long> &dp, int idx) {
+        if(idx >= q.size()) return 0;
+
+        if(dp[idx] != -1) return dp[idx];
+
+        long long take = q[idx][0] + memoization(q, dp, idx + q[idx][1] + 1);
+        long long skip = memoization(q, dp, idx + 1);
+
+        return dp[idx] = max(take, skip);
+    }
+
+    long long mostPoints(vector<vector<int>>& questions) {
+        // return recursion(questions, 0);
+
+        int n = questions.size();
+        vector<long long> dp(n + 1, -1);
+        return memoization(questions, dp, 0);
+    }
+};
