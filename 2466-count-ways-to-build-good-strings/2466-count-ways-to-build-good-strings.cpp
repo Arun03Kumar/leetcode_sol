87d6b0f29951bb872be318880f4ba23fc233cf40
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-08-03 23:41:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int recursion(int low, int high, int zero, int one, string ans) {
+        if(ans.size() > high) return 0;
+        // if(ans.size() >= low && ans.size() <= high) return 1;
+
+        string ones(one, '1');
+        string zeros(zero, '0');
+
+        string temp_zero = ans + zeros;
+        string temp_one = ans + ones;
+
+        int zero_take = 0, one_take = 0;
+        if(temp_one.size() >= low && temp_one.size() <= high) {
+            one_take = 1 + recursion(low, high, zero, one, temp_one);
+        }
+        else {
+            one_take = recursion(low, high, zero, one, temp_one);
+        }
+
+        if(temp_zero.size() >= low && temp_zero.size() <= high) {
+            zero_take = 1 + recursion(low, high, zero, one, temp_zero);
+        }
+        else {
+            zero_take = recursion(low, high, zero, one, temp_zero);
+        }
+
+        return zero_take + one_take;
+    }
+
+    int memoization(int low, int high, int zero, int one, vector<int> &dp, int length) {
+
+        if(length > high) return 0;
+
+        if(dp[length] != -1) return dp[length];
+
+        int should_add_one = 0;
+
+        if(length >= low && length <= high) {
+            should_add_one = 1;
+        }
+
+        int ones = memoization(low, high, zero, one, dp, length + one);
+        int zeros = memoization(low, high, zero, one, dp, length + zero);
+
+        return dp[length] = should_add_one + ones + zeros;
+
+    }
+
+    int countGoodStrings(int low, int high, int zero, int one) {
+        // return recursion(low, high, zero, one, "");
+
+        vector<int> dp(high + 1, -1);
+        return memoization(low, high, zero, one, dp, 0);
+    }
+};
