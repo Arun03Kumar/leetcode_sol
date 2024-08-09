50d8430375b1fdd9e8053c43d7e614bfa6736069
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-08-09 12:55:35 +0530

    Time: 388 ms (33.73%), Space: 179.6 MB (24.83%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,52 +1,73 @@
 class NumArray {
 public:
-    vector<int> tree;
     int n;
+    vector<int> segment_tree;
+
     NumArray(vector<int>& nums) {
         n = nums.size();
-        tree.resize(nums.size() * 2, 0);
-        buildTree(nums);
+        segment_tree.resize(n * 4);
+        build(nums, 0, 0, n - 1);
     }
 
-    void buildTree(vector<int> &nums) {
-        for(int i = n, j = 0; i < 2 * n; i++, j++) {
-            tree[i] = nums[j];
+    void build(vector<int> &nums, int node, int left, int right) {
+
+        if(left == right) {
+            segment_tree[node] = nums[left];
+            return;
         }
-        for(int i = n - 1; i > 0; i--) {
-            tree[i] = tree[i * 2] + tree[i * 2 + 1];
+
+        int mid = left + (right - left) / 2;
+
+        build(nums, node * 2 + 1, left, mid);
+        build(nums, node * 2 + 2, mid + 1, right);
+
+        segment_tree[node] = segment_tree[node * 2 + 1] + segment_tree[node * 2 + 2];
+
+    }
+
+    void update_helper(int node, int left, int right, int index, int val) {
+
+        if(left == right) {
+            segment_tree[node] = val;
+            return;
         }
+
+        int mid = left + (right - left) / 2;
+
+        if(index <= mid) {
+            update_helper(node * 2 + 1, left, mid, index, val);
+        }
+        else {
+            update_helper(node * 2 + 2, mid + 1, right, index, val);
+        }
+
+        segment_tree[node] = segment_tree[node * 2 + 1] + segment_tree[node * 2 + 2];
+
     }
     
     void update(int index, int val) {
-        index += n;
-        tree[index] = val;
-        while(index > 0) {
-            int left = index, right = index;
-            if(index % 2 == 0) right = index + 1;
-            else left = index - 1;
+        update_helper(0, 0, n - 1, index, val);
+    }
 
-            tree[index / 2] = tree[left] + tree[right];
-            index /= 2;
+    int range_helper(int node, int l, int r, int left, int right) {
+
+        if(right < l || r < left) return 0;
+
+        if(l >= left && r <= right) {
+            return segment_tree[node];
         }
+
+        int mid = l + (r - l) / 2;
+
+        int q_l = range_helper(node * 2 + 1, l, mid, left, right);
+        int q_r = range_helper(node * 2 + 2, mid + 1, r, left, right);
+
+        return q_l + q_r;
+
     }
     
-    int sumRange(int l, int r) {
-        l += n;
-        r += n;
-        int sum = 0;
-        while(l <= r) {
-            if((l % 2) == 1) {
-                sum += tree[l];
-                l++;
-            }
-            if((r % 2) == 0) {
-                sum += tree[r];
-                r--;
-            }
-            l /= 2;
-            r /= 2;
-        }
-        return sum;
+    int sumRange(int left, int right) {
+        return range_helper(0, 0, n - 1, left, right);
     }
 };
 
