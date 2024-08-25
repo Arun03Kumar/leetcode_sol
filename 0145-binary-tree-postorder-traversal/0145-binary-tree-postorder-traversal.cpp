d8d16f362a4032dd397af281304a002df72ea175
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-08-25 12:56:32 +0530

    Time: 0 ms (100%), Space: 10.1 MB (60.43%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -17,47 +17,90 @@ public:
         // if top of stack also doesn't contains the right then it means the top of node will go in to our answer.
         // and we also check if it is right child of top element because then top element would also go in answer next, because we have traversed left, right and now root.
         
-        stack<TreeNode*> st;
+        // Approach 1: using one stack
+        // stack<TreeNode*> st;
+        // vector<int> ans;
+
+        // // if(root != nullptr) st.push(root);
+
+        // TreeNode* curr = root;
+
+        // while(curr != NULL || !st.empty()) {
+
+        //     if(curr != nullptr) {
+
+        //         st.push(curr);
+        //         curr = curr -> left;
+
+        //     }
+        //     else {
+
+        //         TreeNode *temp = st.top() -> right;
+
+        //         if(temp == NULL) {
+
+        //             temp = st.top();
+        //             st.pop();
+        //             ans.push_back(temp -> val);
+
+        //             while(!st.empty() && temp == st.top() -> right) {
+        //                 temp = st.top();
+        //                 st.pop();
+        //                 ans.push_back(temp -> val);
+        //             }
+
+        //         }
+        //         else {
+
+        //             curr = temp; 
+
+        //         }
+
+        //     }
+        // }
+
+        // return ans;
+
+
+
+
+        // Approach 2: using two stacks
+        // in this we first push the root in to first stack
+        // then loop over till first stack is not empty
+        // pop the top element then push in to 2nd stack
+        // push the left child of popped element into stack1
+        // push the right child of popped element into stack1;
+
+        // when first stack empty pop all the elements from stack 2 and add into answer. that would be postorder traversal
+
         vector<int> ans;
+        if(root == nullptr) return ans;
+    
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
 
-        // if(root != nullptr) st.push(root);
+        while(!st1.empty()) {
 
-        TreeNode* curr = root;
+            TreeNode *top = st1.top();
+            st1.pop();
+            st2.push(top);
 
-        while(curr != NULL || !st.empty()) {
-
-            if(curr != nullptr) {
-
-                st.push(curr);
-                curr = curr -> left;
-
+            if(top -> left != nullptr) {
+                st1.push(top -> left);
             }
-            else {
 
-                TreeNode *temp = st.top() -> right;
-
-                if(temp == NULL) {
-
-                    temp = st.top();
-                    st.pop();
-                    ans.push_back(temp -> val);
-
-                    while(!st.empty() && temp == st.top() -> right) {
-                        temp = st.top();
-                        st.pop();
-                        ans.push_back(temp -> val);
-                    }
-
-                }
-                else {
-
-                    curr = temp; 
-
-                }
-
+            if(top -> right != nullptr) {
+                st1.push(top -> right);
             }
+
+        }
+
+        while(!st2.empty()) {
+            ans.push_back(st2.top() -> val);
+            st2.pop();
         }
 
         return ans;
+
     }
 };
