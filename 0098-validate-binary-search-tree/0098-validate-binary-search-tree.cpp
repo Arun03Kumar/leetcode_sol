ebd4a71d6959d208330e5f629b3f0f7d9cb37a18
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-08-27 01:59:55 +0530

    Time: 8 ms (56.1%), Space: 20.4 MB (67.28%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -19,14 +19,36 @@ public:
         inorder_traversal(root -> right, v);
 
     }
+
+    bool solve(TreeNode *root, long long left, long long right) {
+        if(!root) return true;
+        if(root -> val <= left || root -> val >= right) return false;
+
+        bool a = solve(root -> left, left, root -> val);
+        bool b = solve(root -> right, root -> val, right);
+
+        return a && b;
+    }
+
     bool isValidBST(TreeNode* root) {
-        vector<int> inorder;
-        inorder_traversal(root, inorder);
+        // vector<int> inorder;
+        // inorder_traversal(root, inorder);
 
-        for(int i = 1; i < inorder.size(); i++) {
-            if(inorder[i] <= inorder[i - 1]) return false;
-        }
+        // for(int i = 1; i < inorder.size(); i++) {
+        //     if(inorder[i] <= inorder[i - 1]) return false;
+        // }
 
-        return true;
+        // return true;
+
+
+        // if we can provide a range to each node then we can compare if current node value lie within that range or not if not then it can't be a BST.
+        // so we start with int min and int max for root
+        // when we go left then range will shrink to the int min to root_val
+        // similarly when we go right then range will shrink to root -> val to the int_max
+
+        long long left = LONG_MIN;
+        long long right = LONG_MAX;
+
+        return solve(root, left, right);
     }
 };
