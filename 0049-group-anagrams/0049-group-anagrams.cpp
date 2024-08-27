882735a1917d39ddd8adfed4dd027b693702586d
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-08-28 01:44:43 +0530

    Time: 53 ms (12.57%), Space: 35.1 MB (6.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,23 +1,47 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // Approach 1: sorting
+        // Approach 1: sorting O(k * nlogn) where k is the number of strings and let n is the size of the longest string
         // just sort each word and add that into the map of it
-        
-        unordered_map<string, vector<string>> mp;
 
-        for(int i = 0; i < strs.size(); i++) {
-            string temp = strs[i];
-            sort(begin(temp), end(temp));
-            mp[temp].push_back(strs[i]);
+    //     unordered_map<string, vector<string>> mp;
+
+    //     for(int i = 0; i < strs.size(); i++) {
+    //         string temp = strs[i];
+    //         sort(begin(temp), end(temp));
+    //         mp[temp].push_back(strs[i]);
+    //     }
+
+    //     vector<vector<string>> ans;
+
+    //     for(auto x: mp) {
+    //         ans.push_back(x.second);
+    //     }
+
+    //     return ans;
+    // }
+
+
+    // Approach 2: it is simillar to the above but we will now store the strings in the array based on indexes because it is given that the all string are lower case letters.
+
+    map<vector<int>, vector<string>> mp;
+
+    for(int i = 0; i < strs.size(); i++) {
+        vector<int> temp(26, 0);
+        for(char x: strs[i]) {
+            temp[x - 'a']++;
         }
 
-        vector<vector<string>> ans;
-
-        for(auto x: mp) {
-            ans.push_back(x.second);
-        }
-
-        return ans;
+        mp[temp].push_back(strs[i]);
     }
+
+    vector<vector<string>> ans;
+
+    for(auto x: mp) {
+        ans.push_back(x.second);
+    }
+
+    return ans;
+    }
+
 };
