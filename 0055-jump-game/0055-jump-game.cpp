c70e800695f81a464081c346021e8ef27980ac33
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-09-10 01:54:37 +0530

    Time: 657 ms (6.92%), Space: 54 MB (7.35%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool recursion(vector<int> &nums, int idx) {
+        if(idx == nums.size() - 1) return true;
+        if(idx >= nums.size()) return false;
+
+        bool temp = false;
+        for(int i = 1; i <= nums[idx]; i++) {
+            temp |= recursion(nums, idx + i);
+        }
+
+        return temp;
+    }
+
+    bool memoization(vector<int> &nums, int idx, vector<int> &dp) {
+        if(idx == nums.size() - 1) return true;
+        if(idx >= nums.size()) return false;
+
+        if(dp[idx] != -1) return dp[idx];
+
+        // bool temp = false;
+        for(int i = 1; i <= nums[idx]; i++) {
+            // temp |= memoization(nums, idx + i, dp);
+            if(memoization(nums, idx + i, dp)) {
+                return dp[idx] = true;
+            }
+        }
+
+        return dp[idx] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        // return recursion(nums, 0);
+
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return memoization(nums, 0, dp);
+    }
+};
