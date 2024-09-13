3584dabf9cc9b964d3253cbcf4c83a50f6db2eb1
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-09-14 02:06:46 +0530

    Time: 3 ms (51.37%), Space: 8.9 MB (17.38%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -45,8 +45,37 @@ public:
         // return recursive(s, 0, 0);
 
         // Approach 2: Memoization
+        // int n = s.size();
+        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        // return memoization(s, dp, 0, 0);
+
+
+        // Approach 3: Bottom-up
         int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return memoization(s, dp, 0, 0);
+        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, false));
+        dp[n][0] = true; // when there is empty string and no open brackets then it will be true this is our base case
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int open = 0; open <= n; open++) {
+
+                bool is_valid = false;
+                if(s[i] == '(') {
+                    is_valid |= dp[i + 1][open + 1];
+                }
+                else if(s[i] == '*') {
+                    is_valid |= dp[i + 1][open + 1];
+                    is_valid |= dp[i + 1][open];
+                    if(open > 0) is_valid |= dp[i + 1][open - 1];
+                }
+                else {
+                    if(open > 0) is_valid |= dp[i + 1][open - 1];
+                }
+
+                dp[i][open] = is_valid;
+            }
+
+        }
+
+        return dp[0][0];
     }
 };
