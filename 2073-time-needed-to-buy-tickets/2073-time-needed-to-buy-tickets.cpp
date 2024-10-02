3f19043c766802cf076c85b03b8721fff588aabf
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-10-02 07:27:57 +0530

    Time: 2 ms (57.04%), Space: 9.7 MB (36.91%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -3,24 +3,46 @@ public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         // Approach 1: Simulation
 
-        int n = tickets.size();
-        queue<pair<int, int>> q;
-        for(int i = 0; i < n; i++) {
-            q.push({i, tickets[i]});
-        }
+        // int n = tickets.size();
+        // queue<pair<int, int>> q;
+        // for(int i = 0; i < n; i++) {
+        //     q.push({i, tickets[i]});
+        // }
 
+        // int ans = 0;
+        // while(!q.empty()) {
+        //     int idx = q.front().first;
+        //     int rem_ticks = q.front().second;
+        //     q.pop();
+        //     ans++;
+
+        //     if(rem_ticks - 1 != 0) {
+        //         q.push({idx, rem_ticks - 1});
+        //     }
+        //     else if(idx == k) {
+        //         break;
+        //     }
+
+        // }
+
+        // return ans;
+
+
+        // Approach 2:
         int ans = 0;
-        while(!q.empty()) {
-            int idx = q.front().first;
-            int rem_ticks = q.front().second;
-            q.pop();
-            ans++;
+        int n = tickets.size();
+        
+        if(tickets[k] == 1) return k + 1;
 
-            if(rem_ticks - 1 != 0) {
-                q.push({idx, rem_ticks - 1});
-            }
-            else if(idx == k) {
-                break;
+        while(tickets[k] > 0) {
+            
+            for(int i = 0; i < n; i++) {
+                if(tickets[i] != 0) {
+                    tickets[i] -= 1;
+                    ans++;
+                }
+
+                if(tickets[k] == 0) return ans;
             }
 
         }
