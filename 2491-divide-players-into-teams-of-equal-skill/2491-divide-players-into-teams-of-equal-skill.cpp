abda087518e0769a28e1348cfaa54b1fd8394881
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-10-04 20:01:27 +0530

    Time: 76 ms (11.4%), Space: 56.5 MB (56.4%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -1,35 +1,57 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
+        // int n = skill.size();
+        // int size = n / 2;
+        // int sum = accumulate(begin(skill), end(skill), 0);
+
+        // if(sum % size != 0) return -1;
+
+        // int target = sum / size;
+
+        // unordered_map<int, int> mp;
+        // long long ans = 0;
+
+        // for(int i = 0; i < n; i++) {
+        //     int rem = target - skill[i];
+
+        //     if(mp.find(rem) == mp.end()) {
+        //         mp[skill[i]]++;
+        //     }
+        //     else {
+        //         ans += skill[i] * rem;
+        //         mp[rem]--;
+        //         cout << rem << " " << mp[rem] << endl;
+        //         if(mp[rem] == 0) {
+        //             mp.erase(rem);
+        //         }
+        //     }
+        // }
+        
+        // if(mp.size() > 0) return -1;
+        // return ans == 0 ? -1 : ans;
+
+
+        // Approach 2: sorting
+
         int n = skill.size();
-        int size = n / 2;
-        int sum = accumulate(begin(skill), end(skill), 0);
+        sort(begin(skill), end(skill));
 
-        if(sum % size != 0) return -1;
-
-        int target = sum / size;
-
-        unordered_map<int, int> mp;
+        int i = 0, j = n - 1;
+        int target = skill[i] + skill[j];
         long long ans = 0;
 
-        for(int i = 0; i < n; i++) {
-            int rem = target - skill[i];
+        while(i < j) {
+            int sum = skill[i] + skill[j];
 
-            if(mp.find(rem) == mp.end()) {
-                mp[skill[i]]++;
-            }
-            else {
-                ans += skill[i] * rem;
-                mp[rem]--;
-                cout << rem << " " << mp[rem] << endl;
-                if(mp[rem] == 0) {
-                    mp.erase(rem);
-                }
-            }
+            if(sum != target) return -1;
+
+            ans += (skill[i] * skill[j]);
+            i++;
+            j--;
         }
-        
-        if(mp.size() > 0) return -1;
-        return ans == 0 ? -1 : ans;
+
+        return ans;
 
     }
 };
