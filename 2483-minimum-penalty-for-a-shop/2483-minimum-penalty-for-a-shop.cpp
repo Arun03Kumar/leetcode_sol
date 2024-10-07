25526c47d94b5edaa881b5e18dc2ad281eb4c916
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-10-08 01:27:03 +0530

    Time: 38 ms (8.08%), Space: 20.4 MB (25.1%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -6,24 +6,72 @@ public:
         // so count number of Y after that index and count number of No before that index
         // but it is also given that shop can closed at n hour so we need to also check in complete array the count of No
 
-        int n = customers.size();
-        int ans = 0, min_penalty = INT_MAX;
-        for(int i = 0; i < n; i++) {
+        // int n = customers.size();
+        // int ans = 0, min_penalty = INT_MAX;
+        // for(int i = 0; i < n; i++) {
             
-            int count = 0;
-            for(int j = i; j < n; j++) {
-                if(customers[j] == 'Y') count++;
-            }
+        //     int count = 0;
+        //     for(int j = i; j < n; j++) {
+        //         if(customers[j] == 'Y') count++;
+        //     }
 
-            for(int j = i - 1;  j >= 0; j--) {
-                if(customers[j] == 'N') count++;
-            }
+        //     for(int j = i - 1;  j >= 0; j--) {
+        //         if(customers[j] == 'N') count++;
+        //     }
 
-            if(count < min_penalty) {
+        //     if(count < min_penalty) {
+        //         ans = i;
+        //         min_penalty = count;
+        //     }
+
+        // }
+
+        // int count = 0;
+        // for(int i = 0; i < n; i++) {
+        //     if(customers[i] == 'N') count++;
+        // }
+
+        // if(count < min_penalty) {
+        //     ans = n;
+        //     min_penalty = count;
+        // }
+
+        // return ans;
+
+        // Approach 2: we can optimize this by calculating the number of N in the left of each char in string
+        // and number of Y in the right side of it;
+        // then simillar to above approach
+        // also consider the last case where take care of whole array
+
+        int n = customers.size();
+        vector<int> left(n, 0), right(n, 0);
+
+        for(int i = 1; i < n; i++) {
+            if(customers[i - 1] == 'N') {
+                left[i] = left[i - 1] + 1;
+            }
+            else {
+                left[i] = left[i - 1];
+            }
+        }
+
+        if(customers[n - 1] == 'Y') right[n - 1] = 1;
+        for(int i = n - 2; i >= 0; i--) {
+            if(customers[i] == 'Y') {
+                right[i] = right[i + 1] + 1;
+            }
+            else {
+                right[i] = right[i + 1];
+            }
+        }
+
+        int ans = 0, penalty = INT_MAX;
+        for(int i = 0; i < n; i++) {
+            int pen = left[i] + right[i];
+            if(pen < penalty) {
+                penalty = pen;
                 ans = i;
-                min_penalty = count;
             }
-
         }
 
         int count = 0;
@@ -31,9 +79,9 @@ public:
             if(customers[i] == 'N') count++;
         }
 
-        if(count < min_penalty) {
+        if(count < penalty) {
             ans = n;
-            min_penalty = count;
+            penalty = count;
         }
 
         return ans;
