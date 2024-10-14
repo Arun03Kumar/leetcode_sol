4e3ccd4d00e77be2215b322512ae860cb8192c6d
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-10-14 19:35:08 +0530

    Time: 16 ms (41.23%), Space: 23.3 MB (78.91%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -11,28 +11,47 @@
  */
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
+    void dfs(TreeNode *root, int curr_depth, int &max_depth, int &ans) {
+        if(!root) return;
 
-        int ans = 0;
-        while(!q.empty()) {
-            TreeNode *front = q.front();
-            q.pop();
-
-            if(front -> right != nullptr) {
-                q.push(front -> right);
-            }
-
-            if(front -> left != nullptr) {
-                q.push(front -> left);
-            }
-
-            if(front -> left == nullptr && front -> right == nullptr) {
-                ans = front -> val;
-            }
+        if(curr_depth > max_depth) {
+            max_depth = curr_depth;
+            ans = root -> val;
         }
 
+        dfs(root -> left, curr_depth + 1, max_depth, ans);
+        dfs(root -> right, curr_depth + 1, max_depth, ans);
+    }
+
+    int findBottomLeftValue(TreeNode* root) {
+        // Approach 1: DFS
+        // at each dfs call we check if current dpepth is greater than the previous depth the first call will always be the left most hence we need not to worry about.
+        int ans = 0;
+        int max_depth = -1;
+        dfs(root, 0, max_depth, ans);
         return ans;
+
+        // queue<TreeNode*> q;
+        // q.push(root);
+
+        // int ans = 0;
+        // while(!q.empty()) {
+        //     TreeNode *front = q.front();
+        //     q.pop();
+
+        //     if(front -> right != nullptr) {
+        //         q.push(front -> right);
+        //     }
+
+        //     if(front -> left != nullptr) {
+        //         q.push(front -> left);
+        //     }
+
+        //     if(front -> left == nullptr && front -> right == nullptr) {
+        //         ans = front -> val;
+        //     }
+        // }
+
+        // return ans;
     }
 };
