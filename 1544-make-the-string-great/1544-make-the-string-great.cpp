8d9b4740327990cb3b6a75fa54c03d8ad77d95fe
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-10-15 04:06:03 +0530

    Time: 4 ms (41.46%), Space: 8 MB (63.37%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     string makeGood(string s) {
-        stack<char> st;
+        // stack<char> st;
+
+        // for(char ch: s) {
+        //     if(!st.empty()) {
+        //         char top = st.top();
+        //         if(top - 32 == ch || top + 32 == ch) st.pop();
+        //         else st.push(ch);
+        //     }
+        //     else st.push(ch);
+        // }
+
+        // string ans = "";
+        // while(!st.empty()) {
+        //     ans += st.top();
+        //     st.pop();
+        // }
+
+        // reverse(begin(ans), end(ans));
+        // return ans;
+
+        // Approach 2:
+        string ans = "";
 
         for(char ch: s) {
-            if(!st.empty()) {
-                char top = st.top();
-                if(top - 32 == ch || top + 32 == ch) st.pop();
-                else st.push(ch);
+            if(ans.size() == 0) ans.push_back(ch);
+            else {
+                char back = ans.back();
+                if(back - 32 == ch || back + 32 == ch) {
+                    ans.pop_back();
+                }
+                else {
+                    ans.push_back(ch);
+                }
             }
-            else st.push(ch);
         }
 
-        string ans = "";
-        while(!st.empty()) {
-            ans += st.top();
-            st.pop();
-        }
-
-        reverse(begin(ans), end(ans));
         return ans;
     }
 };
