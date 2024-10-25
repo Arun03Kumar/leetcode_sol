84db7f339574c6dd5ef9dd85d8c420706f745661
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-10-25 19:26:12 +0530

    Time: 11 ms (96.06%), Space: 31.5 MB (6.49%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,29 +1,22 @@
 class KthLargest {
 public:
-    // here we are removing all the min elements in the priority queue ensuring top element is always the kth largest element.
-    // so at each push in pq we check if its size is greater than k then we pop from the pq.
-    // if in the start there are n elements then adding them and maintaing the pq will cost nlogk time because we are maintaing at most k because at each insert pq will rearrange itself and that takes logk time.
-    // then then are m add operation then it will cost mlogk.
-    // so total (n + m)logk.
     priority_queue<int, vector<int>, greater<int>> pq;
-    int K;
+    int _k;
+
     KthLargest(int k, vector<int>& nums) {
-        K = k;
-        for(int x: nums) {
+        _k = k;
+        for(int &x: nums) {
             pq.push(x);
 
             if(pq.size() > k) {
                 pq.pop();
             }
-        }
+        } 
     }
     
     int add(int val) {
         pq.push(val);
-
-        if(pq.size() > K) {
-            pq.pop();
-        }
+        if(pq.size() > _k) pq.pop();
 
         return pq.top();
     }
