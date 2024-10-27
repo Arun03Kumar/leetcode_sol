e547a86bfcc39ea33ef3781b4a5246d835cc3232
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-10-27 19:54:34 +0530

    Time: 22 ms (9.46%), Space: 30.1 MB (5.31%) - LeetHub

---
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int recursion(vector<vector<int>> &mat, int i, int j) {
+        if(i >= mat.size() || j >= mat[0].size()) return 0;
+
+        if(mat[i][j] == 0) return 0;
+
+        int right = recursion(mat, i, j + 1);
+        int diag = recursion(mat, i + 1, j + 1);
+        int bottom = recursion(mat, i + 1, j);
+
+        return 1 + min({right, diag, bottom});
+    }
+
+    int memoization(vector<vector<int>> &mat, int i, int j, vector<vector<int>> &dp) {
+        if(i >= mat.size() || j >= mat[0].size()) return 0;
+
+        if(mat[i][j] == 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int right = memoization(mat, i, j + 1, dp);
+        int diag = memoization(mat, i + 1, j + 1, dp);
+        int bottom = memoization(mat, i + 1, j, dp);
+
+        return dp[i][j] = 1 + min({right, diag, bottom});
+    }
+
+    int countSquares(vector<vector<int>>& matrix) {
+        // Approach 1: recursion
+        // for each index we are searchin how many ones are ther in 3 directions, right, bottom and in diagonal
+        // for each index we get these three values, so the number of square matrix will be equal to have size minimum to these three which will contains all 1s
+        // but the current index will also be considered as in answer becuse 1 is there
+        // int m = matrix.size();
+        // int n = matrix[0].size();
+
+        // int ans = 0;
+        // for(int i = 0; i < m; i++) {
+        //     for(int j = 0; j < n; j++) {
+
+        //         if(matrix[i][j] == 1) {
+        //             ans += recursion(matrix, i, j);
+        //         }
+
+        //     }
+        // }
+
+        // return ans;
+
+
+        // Approach 2: memoization
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        int ans = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(matrix[i][j] == 1) {
+                    ans += memoization(matrix, i, j, dp);
+                }
+
+            }
+        }
+
+        return ans;
+    }
+};
