a2494f934bfebe073923cfcd7c70853f92eb73e0
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-10-28 23:05:55 +0530

    Time: 0 ms (100%), Space: 7.5 MB (70.86%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 0) return false;
-        
-        while(n != 1) {
-            if(n % 4 == 0) n /= 4;
-            else break;
-        }
+        // if(n == 0) return false;
 
-        return n == 1;
+        // while(n != 1) {
+        //     if(n % 4 == 0) n /= 4;
+        //     else break;
+        // }
+
+        // return n == 1;
+
+        // Approach 2:
+        // we can find out that if a number is power of 2 by (n&(n-1) == 0)
+        // if a number is power of 4 then then number - 1 will be divisible by 3
+        // e.g. 16 -> (16 - 1) = 15 % 3 == 0
+        if(n <= 0) return false;
+        if((n & (n - 1)) == 0 && (n - 1) % 3 == 0) return true;
+        return false;
     }
 };
