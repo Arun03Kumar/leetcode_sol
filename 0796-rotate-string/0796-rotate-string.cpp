474fe93c71c9b4abdee30bead370ca779efa746f
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-11-03 17:41:55 +0530

    Time: 0 ms (100%), Space: 7.5 MB (83.69%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        // Approach 1:
+
+        // int n = s.size();
+        // while(n--) {
+        //     char c = s[0];
+        //     s.erase(0, 1);
+        //     s.push_back(c);
+        //     cout << s << " ";
+        //     if(s == goal) return true;
+        // }
+
+        // return false;
+
+
+        // Approach 2: using rotate function
+        // the args in rotate fn are like this
+        // rotate(start ptr, position which will become the start, end ptr)
         int n = s.size();
-        while(n--) {
-            char c = s[0];
-            s.erase(0, 1);
-            s.push_back(c);
-            cout << s << " ";
+        for(int i = 0; i < n; i++) {
+            rotate(begin(s), begin(s) + 1, end(s));
+
             if(s == goal) return true;
         }
-
         return false;
+
+        
+        // Approach 3:
+        // if we concatanate the string in itself then it will contains all the rotations in at as a substring
+
     }
 };
