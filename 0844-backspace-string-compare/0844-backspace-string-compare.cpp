8dbb068fdb0314d737b520946a5f6ed9a4d3d97e
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-11-18 23:59:49 +0530

    Time: 0 ms (100%), Space: 7.7 MB (95.88%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
-    string removeChars(string s) {
-        int i = 0;
-        while(i < s.size()) {
-            if(s[i] == '#' && i != 0) {
-                s.erase(s.begin() + i - 1, s.begin() + i + 1);
-                i -= 1;
-            }
-            else if(i == 0 && s[i] == '#') {
-                s.erase(0, 1);
-            }
-            else 
-                i++;
-        }
-        return s;
-    }
     bool backspaceCompare(string s, string t) {
-        string ans1 = removeChars(s);
-        string ans2 = removeChars(t);
-        // cout << ans1 << " d " << ans2;
-        return ans1 == ans2 ? true : false;
+        // Approach 2: 
+        // start from the end of both strings and increse the count of #
+        // if we get any character but count of # is still greater than 0 then skip the character
+        // else break that loop
+        // do this for 2nd string 
+        // now in the end compare the characters
+        // if equal then do this for rest characters
+
+        int i = s.size() - 1;
+        int j = t.size() - 1;
+
+        int count_s = 0, countt = 0;
+
+        while(i >= 0 || j >= 0) {
+
+            while(i >= 0) {
+                if(s[i] == '#') {
+                    count_s++;
+                    i--;
+                }
+                else if(count_s > 0) {
+                    count_s--;
+                    i--;
+                }
+                else break;
+
+            }
+
+            while(j >= 0) {
+                if(t[j] == '#') {
+                    countt++;
+                    j--;
+                }
+                else if(countt > 0) {
+                    countt--;
+                    j--;
+                }
+                else break;
+            }
+
+            char first = i < 0 ? '$' : s[i]; 
+            char sec = j < 0 ? '$' : t[j]; 
+
+            if(first != sec) return false;
+
+            i--;
+            j--;
+        }
+
+        return true;
     }
 };
