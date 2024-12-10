731f8b01a47a900d49d853f96f59885953a0179c
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-12-11 02:15:19 +0530

    Time: 12 ms (56%), Space: 10.8 MB (76.12%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,30 +1,55 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        // Brute force - find all substrings and create a map
+        //Approach 1: Brute force - find all substrings and create a map
+        // int n = s.size();
+        // unordered_map<string, int> mp;
+
+        // for(int i = 0; i < n; i++) {
+        //     string curr = "";
+        //     for(int j = i; j < n; j++) {
+        //         if(curr == "" || curr.back() == s[j]) {
+        //             curr.push_back(s[j]);
+        //             mp[curr]++;
+        //         }
+        //         else {
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // int ans = -1;
+        // for(auto [key, val]: mp) {
+        //     if(val >= 3) {
+        //         ans = max(ans, (int)key.size());
+        //     }
+        // }
+
+        // return ans;
+
+        // Approach 2: we can improve the space 
+        // we don't need the actual string, so we can create a map and store the {char, length} pair
         int n = s.size();
-        unordered_map<string, int> mp;
+        map<pair<char, int>, int> mp; // mp[{char, length}] = frequency
 
         for(int i = 0; i < n; i++) {
-            string curr = "";
+            char ch = s[i];
+            int length = 0;
             for(int j = i; j < n; j++) {
-                if(curr == "" || curr.back() == s[j]) {
-                    curr.push_back(s[j]);
-                    mp[curr]++;
+                if(s[j] == ch) {
+                    length++;
+                    mp[{ch, length}]++;
                 }
-                else {
-                    break;
-                }
+                else break;
             }
         }
 
         int ans = -1;
         for(auto [key, val]: mp) {
             if(val >= 3) {
-                ans = max(ans, (int)key.size());
+                ans = max(ans, int(key.second));
             }
         }
-
         return ans;
     }
 };
