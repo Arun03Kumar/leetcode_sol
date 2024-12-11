99ab6a9f67806f50df80be1c07bdb223d69a2a3b
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-12-12 01:25:01 +0530

    Time: 71 ms (49.57%), Space: 105 MB (68.73%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    int binary_search(vector<int> &nums, int k, int idx) {
+        int l = 0, r = nums.size() - 1;
+        int ans = idx;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] <= nums[idx] + 2 * k) {
+                l = mid + 1;
+                ans = mid;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
     int maximumBeauty(vector<int>& nums, int k) {
         // Approach 1: 
         // compute the range or interval by subtracting and adding k, and form an array of pairs
@@ -9,26 +27,38 @@ public:
         // (a, b) and (c, d) if b < c then they are not overlapping
         // so we need to maintain a queue or deque so that we can compare the current pair with the starting one because it may possible that current pair is overlapped with the previous one but not with those who are push initially in the queue
 
+        // int n = nums.size();
+        // vector<pair<int, int>> range;
+
+        // for(int &x: nums) {
+        //     range.push_back({x - k, x + k});
+        // }
+
+        // sort(begin(range), end(range));
+        // int ans = 1;
+
+        // deque<pair<int, int>> deq;
+        // for(pair<int, int> &p: range) {
+        //     while(!deq.empty() && deq.front().second < p.first) {
+        //         deq.pop_front();
+        //     }
+
+        //     deq.push_back(p);
+        //     ans = max(ans, (int)deq.size());
+        // }
+        // return ans;
+
+
+
+        // Approach 2: Binary Search
         int n = nums.size();
-        vector<pair<int, int>> range;
-
-        for(int &x: nums) {
-            range.push_back({x - k, x + k});
-        }
-
-        sort(begin(range), end(range));
+        sort(begin(nums), end(nums));
         int ans = 1;
 
-        deque<pair<int, int>> deq;
-        for(pair<int, int> &p: range) {
-            while(!deq.empty() && deq.front().second < p.first) {
-                deq.pop_front();
-            }
-
-            deq.push_back(p);
-            ans = max(ans, (int)deq.size());
+        for(int i = 0; i < n; i++) {
+            int idx = binary_search(nums, k, i);
+            ans = max(ans, idx - i + 1);
         }
-
 
         return ans;
     }
