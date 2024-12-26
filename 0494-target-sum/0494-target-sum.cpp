04905ccbd6918fd982484001cefa1433621f72f7
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-12-26 16:45:32 +0530

    Time: 367 ms (33.5%), Space: 89.8 MB (6.61%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -13,7 +13,26 @@ public:
         return one + two;
     }
 
+    int memoization(vector<int> &nums, int target, int idx, int curr, unordered_map<string, int> &dp) {
+        if(idx >= nums.size()) {
+            if(curr == target) return 1;
+            
+            return 0;
+        }
+
+        string s = to_string(idx) + '_' + to_string(curr);
+        if(dp.find(s) != dp.end()) return dp[s];
+    
+        int one = memoization(nums, target, idx + 1, curr + nums[idx], dp);
+        int two = memoization(nums, target, idx + 1, curr - nums[idx], dp);
+        
+        return dp[s] = one + two;
+    }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        return recursion(nums, target, 0, 0);
+        // return recursion(nums, target, 0, 0);
+
+        unordered_map<string, int> dp;
+        return memoization(nums, target, 0, 0, dp);
     }
 };
