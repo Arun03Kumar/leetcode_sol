458ea5e5f30aaacbc6f637937694dd39bea53291
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2024-12-31 16:14:09 +0530

    Time: 3 ms (26.6%), Space: 12.7 MB (65.58%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -55,13 +55,63 @@ public:
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
 
+        // we need to check by buying all 3 tickets at each day.
+        // for one day pass its easy, but for 7 day and 30 day we need to check that if current date falls between 7 or 30 days if we buy those tickets.
+
+
         // Approach 1: recursion
         // return recursion(days, costs, 0);
 
 
         // Approach 2: memoization
+        // int n = days.size();
+        // vector<int> dp(n + 1, -1);
+        // return memoization(days, costs, dp, 0);
+
+
+        // Approach 3: bottom up
+
+        // dp[i] this state tells that min amount needed to travel till this date
+        // we can reach at date i if we bought ticket from last day, or i - 7 day or i - 30 day
+        // we need set to find out if we are travelling on day i or not
+
+        unordered_set<int> st;
+        for(int day: days) st.insert(day);
+
         int n = days.size();
-        vector<int> dp(n + 1, -1);
-        return memoization(days, costs, dp, 0);
+        int last_day = days[n - 1];
+
+        vector<int> dp(last_day + 1, INT_MAX);
+        dp[0] = 0;
+
+        for(int i = 1; i <= last_day; i++) {
+            if(st.find(i) == st.end()) {
+                dp[i] = dp[i - 1];
+                continue;
+            }
+
+            int cost_1 = costs[0] + dp[i - 1];
+            int cost_7 = 0, cost_30 = 0;
+            // if(i - 7 >= 0) {
+            //     cost_7 = costs[1] + dp[i - 7];
+            // }
+            // else {
+            //     cost_7 = costs[1] + dp[0];
+            // }
+
+            // we can do this if and else in shortcut as
+            cost_7 = costs[1] + dp[max(0, i - 7)];
+
+            if(i - 30 >= 0) {
+                cost_30 = costs[2] + dp[i - 30];
+            }
+            else {
+                cost_30 = costs[2] + dp[0];
+            }
+
+            dp[i] = min({cost_1, cost_7, cost_30});
+        }
+
+        return dp[last_day];
     }
 };
