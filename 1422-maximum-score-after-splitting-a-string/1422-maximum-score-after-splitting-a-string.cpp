bb842eb232d0b37ff5a76db1115634f0e3886b5e
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-01-01 19:21:46 +0530

    Time: 44 ms (5.34%), Space: 9.6 MB (6.19%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int zeros = 0, ones = 0;
-        for(char &ch: s) {
-            if(ch == '0') zeros++;
-            else ones++;
+        int n = s.size();
+        vector<int> zero(n, 0), ones(n, 0);
+
+        if(s[0] == '0') zero[0] = 1;
+        if(s[n - 1] == '1') ones[n - 1] = 1;
+
+        for(int i = 1; i < n; i++) {
+            if(s[i] == '0') zero[i] = zero[i - 1] + 1;
+            else zero[i] = zero[i - 1];
         }
 
-        int temp_0 = 0, temp_1 = 0;
+        for(int i = n - 2; i >= 0; i--) {
+            if(s[i] == '1') ones[i] = ones[i + 1] + 1;
+            else ones[i] = ones[i + 1];
+        }
+
+        for(int &x: ones) cout << x << " ";
+        cout << endl;
+        for(int &x: zero) cout << x << " ";
+
         int ans = 0;
-        for(int i = 0; i < s.size() - 1; i++) {
-
-            if(s[i] == '0') temp_0++;
-            else temp_1++;
-
-            ans = max(ans, temp_0 + ones - temp_1);
+        for(int i = 0; i < zero.size() - 1; i++) {
+            int temp = (i == n - 1) ? 0 : ones[i + 1];
+            ans = max(ans, zero[i] + temp);
         }
 
         return ans;
