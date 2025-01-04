8a61fe6f272b97965e92e55a00401a7e77428faf
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-01-04 17:45:31 +0530

    Time: 261 ms (50.92%), Space: 15.6 MB (85.89%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -6,37 +6,76 @@ public:
         // we need to just find the starting and ending indexes for a character then we can find the number of characters between them that will make the palindrome
         // but since we want unique palindromes so it may possible that there are more than one characters present which are same so we need to use set
 
+        // int n = s.size();
+        // unordered_set<char> st;
+
+        // for(int i = 0; i < n; i++) {
+        //     st.insert(s[i]);
+        // }
+
+        // int ans = 0;
+
+        // for(char ch: st) {
+
+        //     int start_idx = -1;
+        //     int end_idx = -1;
+
+        //     for(int i = 0; i < n; i++) {
+        //         if(ch == s[i]) {
+        //             if(start_idx == -1) {
+        //                 start_idx = i;
+        //             }
+        //             else {
+        //                 end_idx = i;
+        //             }
+        //         }
+        //     }
+
+        //     unordered_set<char> st2;
+        //     for(int j = start_idx + 1; j < end_idx; j++) {
+        //         st2.insert(s[j]);
+        //     }
+
+        //     ans += st2.size();
+
+        // }
+
+        // return ans;
+
+
+
+        // Approach 2: using vector instead of start_idx and end_idx
+
         int n = s.size();
-        unordered_set<char> st;
+        vector<pair<int, int>> indexes(26, {-1, -1});
 
         for(int i = 0; i < n; i++) {
-            st.insert(s[i]);
-        }
+            int idx = s[i] - 'a';
+
+            if(indexes[idx].first == -1) {
+                indexes[idx].first = i;
+            }
+            else {
+                indexes[idx].second = i;
+            }
+        } 
 
         int ans = 0;
+        for(int i = 0; i < 26; i++) {
+            
+            if(indexes[i].first != -1) {
 
-        for(char ch: st) {
+                int start = indexes[i].first;
+                int end = indexes[i].second;
 
-            int start_idx = -1;
-            int end_idx = -1;
-
-            for(int i = 0; i < n; i++) {
-                if(ch == s[i]) {
-                    if(start_idx == -1) {
-                        start_idx = i;
-                    }
-                    else {
-                        end_idx = i;
-                    }
+                unordered_set<char> st;
+                for(int j = start + 1; j < end; j++) {
+                    st.insert(s[j]);
                 }
-            }
 
-            unordered_set<char> st2;
-            for(int j = start_idx + 1; j < end_idx; j++) {
-                st2.insert(s[j]);
-            }
+                ans += st.size();
 
-            ans += st2.size();
+            }
 
         }
 
