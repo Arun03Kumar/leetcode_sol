15d31a5237494285dc17f76ad5590efbe4b91a01
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-02-25 17:25:39 +0530

    Time: 21 ms (10.71%), Space: 75.8 MB (7.14%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var numOfSubarrays = function(arr) {
+    // basically when we create prefix array we get the sum if sum is odd then we get an integer which sums with previous even numbers to get odd number
+    // vice versa is also true
+
+    let mod = 1e9 + 7;
+
+    let n = arr.length;
+    let prefix = []
+    prefix.push(arr[0]);
+
+    for(let i = 1; i < n; i++) {
+        prefix.push(prefix[i - 1] + arr[i]);
+    }
+
+    let oddCount = 0, evenCount = 1;
+    let ans = 0;
+
+    for(let x of prefix) {
+        if(x % 2 == 0) {
+            ans += oddCount;
+            evenCount++;
+        }
+        else {
+            ans += evenCount;
+            oddCount++;
+        }
+    }
+
+    return ans % mod;
+};
