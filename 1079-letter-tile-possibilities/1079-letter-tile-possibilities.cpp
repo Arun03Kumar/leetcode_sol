7814c8613df5c60801c92a447634ea53933ff475
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-02-25 18:37:49 +0530

    Time: 39 ms (28.96%), Space: 18 MB (18.13%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int n;
+    void solve(string &tiles, vector<bool> &used, unordered_set<string> &st, string curr) {
+        st.insert(curr);
+        for(int i = 0; i < n; i++) {
+            if(used[i]) {
+                continue;
+            }
+
+            used[i] = true;
+            curr.push_back(tiles[i]);
+
+            solve(tiles, used, st, curr);
+
+            used[i] = false;
+            curr.pop_back();
+        }
+    }
+    int numTilePossibilities(string tiles) {
+        n = tiles.size();
+        vector<bool> used(n, false);
+        unordered_set<string> st;
+        string curr = "";
+        solve(tiles, used, st, curr);
+        return st.size() - 1;
+    }
+};
