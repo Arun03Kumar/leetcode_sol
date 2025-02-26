c9b3c75653b765f83032727f2b957e1ce2685cec
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-02-27 00:12:05 +0530

    Time: 0 ms (100%), Space: 14.6 MB (72.56%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        vector<int> ans;
+        // vector<int> ans;
+        // int n = nums.size();
+
+        // set<int> st;
+        // for(int i = 0; i < n; i++) {
+        //     if(nums[i] == key) st.insert(i);
+        // }
+
+        // for(int i = 0; i < n; i++) {
+        //     for(int x: st) {
+        //         if(abs(i - x) <= k) {
+        //             ans.push_back(i);
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // sort(begin(ans), end(ans));
+        // return ans;
+
+
+        // Approach 2:
+        // search for key, then go to k elements left and right of it and store the index in the array
         int n = nums.size();
-
-        set<int> st;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == key) st.insert(i);
-        }
+        vector<int> ans;
 
         for(int i = 0; i < n; i++) {
-            for(int x: st) {
-                if(abs(i - x) <= k) {
-                    ans.push_back(i);
-                    break;
+            if(nums[i] == key) {
+                int left = max(0, i - k);
+                int right = min(i + k, n - 1);
+
+                if(ans.size() != 0) {
+                    left = max(left, ans.back() + 1); // this is the case for overlapping 
+                }
+
+                for(int j = left; j <= right; j++) {
+                    ans.push_back(j);
                 }
             }
         }
 
-        sort(begin(ans), end(ans));
         return ans;
     }
 };
