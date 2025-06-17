7a2d07924c5564a0f4de18068649b3424a983cca
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-06-17 16:38:55 +0530

    Time: 4 ms (60.64%), Space: 26.6 MB (35.26%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    bool check(string &s, int i, int j) {
-        while(i <= j) {
-            if(s[i] == s[j]) {
-                i++;
-                j--;
-            }
-            else return false;
+    bool solve(string s, int i, int j) {
+        while(i < j) {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
         }
         return true;
     }
-    bool validPalindrome(string s) {
-        // we use two pointer approach we start from both ends and if characters are same we move to next characters
-        // but if a character is not matched we can check if remaining characters will form palindrome or not because before those string will be palindrome because then only we have came to that point
 
+    bool validPalindrome(string s) {
         int i = 0, j = s.size() - 1;
         while(i <= j) {
-            if(s[i] == s[j]) {
+            if(s[i] != s[j]) {
+                return solve(s, i + 1, j) || solve(s, i, j - 1);
+            }
+            else {
                 i++;
                 j--;
             }
-            else {
-                bool x = check(s, i + 1, j); // exclude i, and check remaining 
-                bool y = check(s, i, j - 1);// exlcude j and check remaining
-                
-                // since we can remove only at most time hence we are returning directly here, even if it not palindrome after excluding
-                return x || y;
-            }
-
         }
 
         return true;
