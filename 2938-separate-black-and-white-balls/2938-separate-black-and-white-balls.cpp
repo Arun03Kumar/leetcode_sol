b9941cb58e8d3658483db2127bbbe949371a9e4e
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-06-19 09:51:03 +0530

    Time: 4 ms (74.58%), Space: 17 MB (90.7%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int j = s.size() - 1;
-        while(j >= 0 && s[j] == '1') j--;
-        // j--;
-        int i = j - 1;
-        long long ans = 0;
-        while(i >= 0) {
-            if(s[i] == '0') i--;
-            else {
-                ans += j - i;
-                j--;
-                i--;
+        long long ans = 0, curr = 0;
+        int n = s.size();
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == '0') {
+                curr++;
             }
+            else ans += curr;
         }
 
         return ans;
     }
 };
+
+// 10001100
+// 01001100
+// 00101100
+// 00011100
+// 00011010
+// 00011001
+// 00010101
+// 00010011
+// 00001011
+// 00000111
