33f0582a121bbd0265c0bf08b867646ea65ad720
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-08-25 13:23:36 +0530

    Time: 1 ms (82.36%), Space: 27.6 MB (17.5%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,47 +11,29 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int in_start, int in_end, int post_start, int post_end) {
+    TreeNode* solve(vector<int> &postorder, int &postorderIdx, int subtreeStartIdx, int subtreeEndIdx, unordered_map<int, int> &mp) {
+        if(subtreeStartIdx > subtreeEndIdx) return nullptr;
 
-        if(in_start > in_end) return nullptr;
+        int rootVal = postorder[postorderIdx];
+        TreeNode* root = new TreeNode(rootVal);
 
-        TreeNode *root = new TreeNode(postorder[post_end]);
-        
-        int i = in_start;
-        for(; i <= in_end; i++) {
-            if(inorder[i] == root -> val) {
-                break;
-            }
-        }
+        postorderIdx--;
 
-        int left_size = i - in_start;
-        int right_size = in_end - i;
+        int inorderRootIdx = mp[rootVal];
 
-        root -> left = solve(inorder, postorder, in_start, i - 1, post_start, post_start + left_size - 1);
-        root -> right = solve(inorder, postorder, i + 1, in_end, post_end - right_size, post_end - 1);
+        root -> right = solve(postorder, postorderIdx, inorderRootIdx + 1, subtreeEndIdx, mp);
+        root -> left = solve(postorder, postorderIdx, subtreeStartIdx, inorderRootIdx - 1, mp);
 
         return root;
-
     }
 
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        // we know that last index of postorder array will contain the root of the tree
-        // so when we get the root we need to find the left subtree and right subtree
-        // with the help of indexings we can get the required positions
-        // for each recursive calls we need the left subtree and right subtree
+        unordered_map<int, int> mp;
+        for(int i = 0; i < inorder.size(); i++) {
+            mp[inorder[i]] = i;
+        }
 
-        // we need 4 pointers in_start, in_end, post_start, post_end
-        // we search the post_end value in inorder array
-        // so starting from in_start to that index will become the left subtree and and i + 1 to in_end will become the right subtree
-        // we can find the number of elements in the left and right subtree 
-        // so in left subtree we send post_start and post_start + left_size
-        // in for right child we send post_end - right_size to post_end - 1
-        
-        int n = inorder.size();
-
-        int in_start = 0, in_end = n - 1;
-        int post_start = 0, post_end = n - 1;
-
-        return solve(inorder, postorder, in_start, in_end, post_start, post_end);
+        int postorderIdx = postorder.size() - 1;
+        return solve(postorder, postorderIdx, 0, postorder.size() - 1, mp);
     }
 };
