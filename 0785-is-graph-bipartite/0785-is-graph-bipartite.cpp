20425b54694b9240fef60bf98a4d12593502ec45
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-09-05 13:25:24 +0530

    Time: 0 ms (100%), Space: 18.9 MB (7.56%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,67 +1,33 @@
 class Solution {
 public:
-    bool DFS(vector<vector<int>> adj, vector<int> &colors, int node, int node_color) {
-        colors[node] = node_color;
-        for(int x: adj[node]) {
-            if(colors[x] == node_color) {
-                return false;
-            }
-            if(colors[x] == -1) {
-                if(!DFS(adj, colors, x, 1 - node_color)) return false;
-            }
+    bool DFS(unordered_map<int, vector<int>> &adj, int node, int val, vector<int> &color) {
+        if(color[node] != -1) return true;
+
+        color[node] = val;
+
+        for(int &nbr: adj[node]) {
+            if(color[nbr] == val) return false;
+            bool x = DFS(adj, nbr, 1 - val, color);
+            if(!x) return false;
         }
-        return true;
-    }
 
-    bool BFS(vector<vector<int>> adj, vector<int> &colors, int node, int node_color) {
-        queue<int> q;
-        q.push(node);
-        colors[node] = node_color;
-
-        while(!q.empty()) {
-            int front = q.front();
-            q.pop();
-            
-            for(int x: adj[front]) {
-
-                if(colors[x] == colors[front]) return false;
-
-                if(colors[x] == -1) {
-                    q.push(x);
-                    colors[x] = 1 - colors[front];
-                }
-            }
-        }
         return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
-        // unordered_map<int, vector<int>> adj;
-        // for(int i = 0; i < graph.size(); i++) {
-        //     for(int x: graph[i]) {
-        //         adj[i].push_back(x);
-        //     }
-        // }
-
-        // no need to create an unordred map its already in the adjecency list form.
-
-        //DFS approach
-
-        // vector<int> colors(graph.size(), -1);
-        // for(int i = 0; i < graph.size(); i++) {
-        //     if(colors[i] == -1) {
-        //         if(!DFS(graph, colors, i, 0)) return false;
-        //     }
-        // }
-        // return true;
-
-        //BFS approach
-        vector<int> colors(graph.size(), -1);
+        unordered_map<int, vector<int>> adj;
         for(int i = 0; i < graph.size(); i++) {
-            if(colors[i] == -1) {
-                if(!BFS(graph, colors, i, 0)) return false;
+            adj[i] = graph[i];
+        }
+
+        int n = graph.size();
+        vector<int> color(n, -1);
+        for(int i = 0; i < n; i++) {
+            if(color[i] == -1) {
+                bool x = DFS(adj, i, 0, color);
+                if(!x) return false;
             }
         }
-        return true; 
+        return true;
     }
 };
