2531f376141ffff621fbdf7485cb7f1cbc38eb92
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-09-06 10:08:34 +0530

    Time: 0 ms (100%), Space: 8.7 MB (68.28%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    long long natural_sum(int x) {
-        return (x) * 1LL * (x + 1) * 1LL / 2;
+    long long naturalSum(int x) {
+        long long ans = x * 1LL * (x + 1) / 2;
+        return ans;
     }
+
     int arrangeCoins(int n) {
-     int low = 1, high = n, ans = 0;
-     while(low <= high) {
-        int mid = low + (high - low) / 2;
-        long long sum = natural_sum(mid);
-        if(sum > n) {
-            high = mid - 1;
+        int l = 1, r = n;
+        int ans = 0;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(naturalSum(mid) <= n) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
         }
-        else {
-            low = mid + 1;
-            ans = max(mid, ans);
-        }
-     } 
-     return ans;  
+
+        return ans;
     }
 };
