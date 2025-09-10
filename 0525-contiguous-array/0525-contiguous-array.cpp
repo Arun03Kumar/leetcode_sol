bea625eb0ae558dab3e80d185ccee571db666b05
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-09-10 12:30:31 +0530

    Time: 28 ms (35.68%), Space: 88.2 MB (71.7%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        // if we treat 0 as -1 then we need to find the subarray with target sum 0, now this problem is similar to leetcode 560
-        // so we find the current sum till index i and we try to find if we have already seen this current sum in the previous array, if yes it means from that index to till this index sum of elements would be zero, because then only we can get the same sum
-        // then find the window size and update the answer if it is max than prev
-        int n = nums.size();
         unordered_map<int, int> mp;
+        int curr_sum = 0, ans = 0;
         mp[0] = -1;
 
-        int curr_sum = 0;
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == 0) curr_sum -= 1;
-            else curr_sum += 1;
+        for(int i = 0; i < nums.size(); i++) {
+            curr_sum += (nums[i] == 0) ? -1 : 1;
+            if(mp.find(curr_sum) != mp.end()) {
+                int x = mp[curr_sum];
+                ans = max(ans, i - x);
+            }
 
             if(mp.find(curr_sum) == mp.end()) {
                 mp[curr_sum] = i;
-            }
-            else {
-                int window_size = i - mp[curr_sum];
-                ans = max(ans, window_size);
             }
         }
 
