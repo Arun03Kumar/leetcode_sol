eb407069233cc9f634d6dcd47a48c2b464c0b89d
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2025-09-21 00:26:04 +0530

    Time: 71 ms (74.44%), Space: 46.3 MB (100%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // so we store array elements in unordered_Set to search them in O(1)
-        // so for getting a sequqnec we just need to check if array contains one previous element and one next element in the array
-        // so we keep on checking this for left and right
-
         if(nums.size() == 0) return 0;
-        unordered_set<int> st(nums.begin(), nums.end());
-
-        int ans = 1;
-        for(int i = 0; i < nums.size(); i++) {
-            int prev = nums[i] - 1, next = nums[i] + 1;
-
-            // while(st.find(prev) != st.end()) prev -= 1;
-            int temp = 1;
-            if(st.find(prev) == st.end()) {
-                while(st.find(next) != st.end()) {
-                    next += 1;
-                    temp++;
-                }
-            }
-
-            ans = max(ans, temp);
-        }
-
-        return ans;
+      if(nums.size() == 1) return 1;
+      sort(nums.begin(), nums.end());
+      int count = 1, ans = 1;
+      for(int i = 1; i < nums.size(); i++) {
+        if(nums[i] == nums[i - 1]) continue;
+        if(nums[i] - nums[i - 1] == 1) count++;
+        else count = 1;
+        ans = max(ans, count);
+      }
+      return ans;
     }
 };
