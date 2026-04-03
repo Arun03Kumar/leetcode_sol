f9222d09d281b99a284063280b95030433da3b6e
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2026-04-03 12:50:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,8 +4,8 @@ public:
         int l = 0, r = x;
         while(l <= r) {
             int mid = l + (r - l) / 2;
-            if(1LL * mid * mid == x * 1LL) return mid;
-            else if(1LL * mid * mid < x) l = mid + 1;
+            if(mid == x / mid) return mid;
+            else if(mid < x / mid) l = mid + 1;
             else r = mid - 1;
         }
         return l - 1;
