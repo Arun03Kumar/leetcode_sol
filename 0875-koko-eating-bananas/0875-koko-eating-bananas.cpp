3df57de31a488ce34599e63dc44bd118e525e4ec
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2026-04-04 15:39:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    long long time_taken(vector<int> &p, int mid) {
-        long long ans = 0;
-        for(int x: p) {
-            int temp = x / mid;
-            ans += (x % mid == 0 ? temp : temp + 1);
+    long long hoursNeeded(vector<int>& piles, int k) {
+        long long total = 0;
+        for (int x : piles) {
+            // int temp = x / mid;
+            // ans += (x % mid == 0 ? temp : temp + 1);
+            total += (x + k - 1) / k; // ceil(x / k)
         }
-        return ans;
+        return total;
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
-        int max_num = INT_MIN;
-        for(int x: piles) max_num = max(x, max_num);
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+        int ans = r;
 
-        int l = 1, r = max_num;
-        int ans = max_num;
-        while(l <= r) {
+        while (l <= r) {
             int mid = l + (r - l) / 2;
-            // cout << mid << endl;
 
-            long long val = time_taken(piles, mid);
-            // cout << val << " ";
-            if(val <= h) {
-                ans = mid;
+            if (hoursNeeded(piles, mid) <= h) {
+                ans = mid;        // valid → try smaller
                 r = mid - 1;
+            } else {
+                l = mid + 1;      // too slow → increase speed
             }
-            else l = mid + 1;
         }
 
         return ans;
