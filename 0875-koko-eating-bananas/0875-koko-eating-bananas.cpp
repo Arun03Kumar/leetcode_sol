953d91d8e1bd5439e93d92602ef6e1eeeb9c1e32
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2026-04-04 15:18:22 +0530

    Time: 5 ms (79.41%), Space: 22.9 MB (45.39%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    bool check(vector<int> arr, int h, int k) {
-        long long tot_hours = 0;
-        for(int x: arr) {
-            if(x % k == 0)
-                tot_hours += x / k;
-            else
-                tot_hours += x / k + 1;
-            // tot_hours += ceil(x / k);
+    long long time_taken(vector<int> &p, int mid) {
+        long long ans = 0;
+        for(int x: p) {
+            int temp = x / mid;
+            ans += (x % mid == 0 ? temp : temp + 1);
         }
-        return tot_hours <= h;
+        return ans;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        int max_ = -1;
-        for(int x: piles) {
-            max_ = max(max_, x);
-            // sum += x;
-        }
-        int low = 1, high = max_, ans = max_;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(check(piles, h, mid)) {
+        int max_num = INT_MIN;
+        for(int x: piles) max_num = max(x, max_num);
+
+        int l = 1, r = max_num;
+        int ans = max_num;
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            // cout << mid << endl;
+
+            long long val = time_taken(piles, mid);
+            // cout << val << " ";
+            if(val <= h) {
                 ans = mid;
-                high = mid - 1;
+                r = mid - 1;
             }
-            else {
-                low = mid + 1;
-            }
+            else l = mid + 1;
         }
+
         return ans;
     }
 };
