5f61c66e24dc32b2c9c7ff3eda1d16ec877b9837
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2026-04-06 10:11:31 +0530

    Time: 11 ms (67.07%), Space: 35 MB (87.3%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    bool can_ship(vector<int> w, int d, int m) {
-        int curr_size = m, days = 1;
+    int find_days(vector<int> &w, int mid) {
+        int count = 0, ans = 1;
         for(int x: w) {
-            if(curr_size - x < 0) {
-                days += 1;
-                curr_size = m;
-            }
-            curr_size -= x;
-        }
-        return days <= d;
-    }
-    int shipWithinDays(vector<int>& weights, int days) {
-        // if ship capacity is smaller than any of the packages weight then it won't be possible to ship so minimum value should be the maximum weight of the package and the maximum capacity of the ship should be sumof all the packages weight, now we got the upper and lower range in which we can do binary search, and for each mid value we can see if it is possible to ship all in the given days, if it possible then we can check if any other value which is smaller is present.
-        int sum = 0, max_elem = -1;
-        for(int x: weights) {
-            sum += x;
-            max_elem = max(x, max_elem);
-        }
-        int low = max_elem, high = sum, ans = sum;
-        while(low <= high) {
-            int mid = (low + high) / 2;
-            if(can_ship(weights, days, mid)) {
-                ans = min(ans, mid);
-                high = mid - 1;
+            if(count + x <= mid) {
+                count += x;
             }
             else {
-                low = mid + 1;
+                ans++;
+                count = x;
             }
+        }
+        return ans;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l = *max_element(begin(weights), end(weights));
+        int r = accumulate(begin(weights), end(weights), 0);
+        int ans = r;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            int val = find_days(weights, mid);
+
+            if(val <= days) {
+                ans = mid;
+                r = mid - 1;
+            }
+            else l = mid + 1;
         }
         return ans;
     }
