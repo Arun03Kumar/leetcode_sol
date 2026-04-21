80faad21f8c16d55193e51f1e9dd7548ead83a0d
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2026-04-21 12:49:54 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int low_outer = 0, high_outer = matrix.size() - 1;
-        while(low_outer <= high_outer) {
-            int mid_outer = low_outer + (high_outer - low_outer) / 2;
-            int size = matrix[mid_outer].size();
-            if(target > matrix[mid_outer][size - 1]) {
-                low_outer = mid_outer + 1;
+        int m = matrix.size(), n = matrix[0].size();
+
+        int l = 0, r = m - 1;
+        int row = -1;
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(matrix[mid][0] <= target && matrix[mid][n - 1] >= target) {
+                row = mid;
+                break;
             }
-            else if(target < matrix[mid_outer][0]) {
-                high_outer = mid_outer - 1;
-            }
-            else break;
+            else if(matrix[mid][0] > target) r = mid - 1;
+            else l = mid + 1;
         }
-        if(low_outer > high_outer) return false;
-        int mid_outer = low_outer + (high_outer - low_outer) / 2;
-        int low_inner = 0, high_inner = matrix[0].size() - 1;
-        while(low_inner <= high_inner) {
-            int mid_inner = low_inner + (high_inner - low_inner) / 2;
-            if(target == matrix[mid_outer][mid_inner])
-                return true;
-            else if(target > matrix[mid_outer][mid_inner]) {
-                low_inner = mid_inner + 1;
-            }
-            else {
-                high_inner = mid_inner - 1;
-            }
+
+        if(row == -1) return false;
+
+        l = 0, r = n - 1;
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(matrix[row][mid] == target) return true;
+            else if(matrix[row][mid] > target) r = mid - 1;
+            else l = mid + 1;
         }
         return false;
     }
