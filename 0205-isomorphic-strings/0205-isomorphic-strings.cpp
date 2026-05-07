54f1548db2c0b012884df8a82ef07e63c2a50a47
ARUN KUMAR <99112516+Arun03Kumar@users.noreply.github.com>
2026-05-07 09:58:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        // so we need to maintian two maps one store the forward mapping and one stores the backward mapping because it may possible that there is a case where two characters are mapping to same element.
-        
-        unordered_map<char, char> forward_mapping, reverse_mapping;
+        unordered_map<char, char> f_map, b_map;
 
         for(int i = 0; i < s.size(); i++) {
-            char ch1 = s[i];
-            char ch2 = t[i];
+            int ch1 = s[i];
+            int ch2 = t[i];
 
-            if(forward_mapping.find(ch1) != forward_mapping.end() && forward_mapping[ch1] != ch2 || reverse_mapping.find(ch2) != reverse_mapping.end() && reverse_mapping[ch2] != ch1) {
-                return false;
+            if(f_map.count(ch1)) {
+                if(f_map[ch1] != ch2) return false;
             }
+            else f_map[ch1] = ch2;
 
-            forward_mapping[ch1] = ch2;
-            reverse_mapping[ch2] = ch1;
+            if(b_map.count(ch2)) {
+                if(b_map[ch2] != ch1) return false;
+            }
+            else b_map[ch2] = ch1;
         }
-
         return true;
     }
 };
